b8e677f7364d15d73eb97b2c531286a46eeca36a
ianpodziewski <ian_podziewski@kenan-flagler.unc.edu>
2025-03-02 12:14:15 -0500

    Update ui.ts - Named Items
    Can easily change if needed

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `AI Assistant for Offshore Workforce Efficiency and Support`;
+export const CHAT_HEADER: string = `Real Estate Loan Underwriting Assistant`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Customized AI by Ian Podziewski`;
 export const CLEAR_BUTTON_TEXT: string = `Clear Inputs & Responses`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const PAGE_TITLE: string = `RE UW Assistant`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
