16eb825d8c805ca789e5498dce4872a9dd54844d
ianpodziewski <ian_podziewski@kenan-flagler.unc.edu>
2025-03-04 09:00:08 -0500

    Update next.config.mjs - pdf-parse issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    // Only apply this for the server build
+    if (isServer) {
+      // In case config.externals doesn't exist, spread it into an array
+      config.externals = [...(config.externals || []), "pdf-parse"];
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
