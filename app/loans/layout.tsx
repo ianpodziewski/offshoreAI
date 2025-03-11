be03c8c38597afba9a9898abb94324a1668a9296
ianpodziewski <Ian_Podziewski@kenan-flagler.unc.edu>
2025-03-10 21:12:47 -0400

    removing duplicate footer

--- app/loans/layout.tsx
+++ app/loans/layout.tsx
@@ -1,11 +1,10 @@
 // app/loans/layout.tsx
 import React from 'react';
-import LayoutWrapper from '../layout-wrapper';
 
 export default function LoansLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <LayoutWrapper>{children}</LayoutWrapper>;
+  return <>{children}</>;
 }
